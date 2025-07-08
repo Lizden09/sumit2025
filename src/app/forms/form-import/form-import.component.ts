ec5c876af475594ec65b2c0061d3492ffdbdceae
Lizden09 <lruanom5@miumg.edu.gt>
2025-07-08 17:05:00 -0600

    implementacion de cuadros de dialogo

---
+++ src/app/forms/form-import/form-import.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-import',
+  imports: [],
+  templateUrl: './form-import.component.html',
+  styleUrl: './form-import.component.css'
+})
+export class FormImportComponent {
+
+}
--- src/app/modules/imports/imports.component.ts
+++ src/app/modules/imports/imports.component.ts
@@ -1,12 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { FormImportComponent } from '../../forms/form-import/form-import.component';
 
 @Component({
   selector: 'app-imports',
-  imports: [CommonModule],
+  imports: [CommonModule, MatDialogModule, FormImportComponent],
   templateUrl: './imports.component.html',
   styleUrl: './imports.component.css'
 })
 export class ImportsComponent {
 
+constructor(private dialog: MatDialog) {}
+
+  openModal() {
+  this.dialog.open(FormImportComponent, {
+    width: '600px',
+    data: {} // puedes pasar datos si es necesario
+  });
+}
 }
