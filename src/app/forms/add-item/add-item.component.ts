8188cf4b614fb9b3a02b5d5b50fe2c3920ca1144
Lizden09 <lruanom5@miumg.edu.gt>
2025-07-22 17:02:08 -0600

    add item component

--- src/app/forms/add-item/add-item.component.ts
+++ src/app/forms/add-item/add-item.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-item',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './add-item.component.html',
   styleUrl: './add-item.component.css'
 })
 export class AddItemComponent {
+  addItemForm!: FormGroup;
+  private fb = inject(FormBuilder);
+
+  Items = [
+    { id: 1, item: 'Escritorio' },
+    { id: 2, item: 'Laptop' },
+    { id: 3, item: 'Cargador' }
+  ];
+
+  constructor() {
+    this.addItemForm = this.fb.group({
+      tipoItem: ['', Validators.required],
+    });
+  }
 
 }
