29b5e9413984e887b5243f9f3e7df30176391c1c
Lizden09 <lruanom5@miumg.edu.gt>
2025-07-23 16:53:34 -0600

    add items cambios

--- src/app/forms/add-item/add-item.component.ts
+++ src/app/forms/add-item/add-item.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-item',
@@ -13,6 +13,14 @@ export class AddItemComponent {
   addItemForm!: FormGroup;
   private fb = inject(FormBuilder);
 
+  codigo = '';
+  mensaje = '';
+  valido = false;
+
+  codigoLinea = '';
+  mensajeLinea = '';
+  validoLinea = false;
+
   Items = [
     { id: 1, item: 'Escritorio' },
     { id: 2, item: 'Laptop' },
@@ -21,8 +29,55 @@ export class AddItemComponent {
 
   constructor() {
     this.addItemForm = this.fb.group({
-      tipoItem: ['', Validators.required],
+      itemType: ['', Validators.required],
+      importCode: ['', Validators.required],
+      lineCode: ['', Validators.required],
+      itemQuantity: [{ value: '', disabled: true }, Validators.required], // deshabilitado desde el inicio
+      marca: ['', Validators.required],
+      modelo: ['', Validators.required]
     });
+  }
+
+  verificarCodigo() {
+    const codigo = this.addItemForm.get('importCode')?.value;
+    const codigosValidos = ['ABC123', 'IMP001', 'XYZ789'];
+
+    if (!codigo || codigo.trim() === '') {
+      this.mensaje = '';
+      return;
+    }
+
+    if (codigosValidos.includes(codigo.trim())) {
+      this.mensaje = 'Código verificado';
+      this.valido = true;
+    } else {
+      this.mensaje = 'Ingrese un código correcto';
+      this.valido = false;
+    }
+  }
+
+  verificarCodigoLinea() {
+    const codigoLinea = this.addItemForm.get('lineCode')?.value;
+    const codigosValidosLinea = ['LN456', 'LINEA22', 'LN999'];
+
+    if (!codigoLinea || codigoLinea.trim() === '') {
+      this.mensajeLinea = '';
+      return;
+    }
+
+    if (codigosValidosLinea.includes(codigoLinea.trim())) {
+      this.mensajeLinea = 'Línea verificada';
+      this.validoLinea = true;
+    } else {
+      this.mensajeLinea = 'Ingrese un número de línea correcto';
+      this.validoLinea = false;
+    }
+  }
+
+
+
+  habilitarCantidad() {
+    this.addItemForm.get('itemQuantity')?.enable();
   }
 
 }
